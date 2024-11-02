a78a5bf86b8f12719ae930c188dd79ffedfcdc48
Rafael Santos <rafaelsantoslp11@gmail.com>
2024-11-02 12:21:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const toggleModal = () => {
     [modal, fade].forEach((el) => el.classList.toggle("hide"));
 }
 
-[openModalButton, closeModalButton, fade].forEach((el) =>{
+[openModalButton, closeModalButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggleModal())
 })
 
@@ -17,3 +17,33 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
+
+
+
+var carousel = $(".carousel"),
+    items = $(".item"),
+    currdeg = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e) {
+    if (e.data.d == "n") {
+        currdeg = currdeg - 60;
+    }
+    if (e.data.d == "p") {
+        currdeg = currdeg + 60;
+    }
+    carousel.css({
+        "-webkit-transform": "rotateY(" + currdeg + "deg)",
+        "-moz-transform": "rotateY(" + currdeg + "deg)",
+        "-o-transform": "rotateY(" + currdeg + "deg)",
+        "transform": "rotateY(" + currdeg + "deg)"
+    });
+    items.css({
+        "-webkit-transform": "rotateY(" + (-currdeg) + "deg)",
+        "-moz-transform": "rotateY(" + (-currdeg) + "deg)",
+        "-o-transform": "rotateY(" + (-currdeg) + "deg)",
+        "transform": "rotateY(" + (-currdeg) + "deg)"
+    });
+}
